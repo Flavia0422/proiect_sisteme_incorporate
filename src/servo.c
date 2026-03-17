8aad6feac893c5db6322ca50532de62ab1f8104b
Flavia0422 <flaviaantonie1306@gmail.com>
2026-03-17 22:35:50 +0200

    Updated project files

---
+++ src/servo.c
@@ -0,0 +1,25 @@
+#include "utils.h"
+#include "delay.h"
+#include "drivers/gpio/gpio.h"
+#include "drivers/pwm/pwm.h"
+#include "bsp/nano.h"
+#include "utils/delay.h"
+int main(void) {
+    PWM_Init(GPIO_PORTB, 1, 50);
+
+    uint8_t duty = 13; // Start at 0°
+    int8_t step = 2;
+
+    while(1) {
+        PWM_SetDutyCycle(GPIO_PORTB, 1, duty);
+        Delay(500); // Pauză lungă pentru mișcare lină
+
+        duty += step;
+
+        if (duty >= 26) { // 180°
+            step = -2;
+        } else if (duty <= 13) { // 0°
+            step = 2;
+        }
+    }
+}
