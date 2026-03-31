7d3672b838fd70f92a6801e6fe404c514471ab34
Flavia0422 <flaviaantonie1306@gmail.com>
2026-03-31 23:11:43 +0300

    Revert "testare componente"
    This reverts commit 2638cab4766294bcf98e9c8bfc19bedf2837d4a9, reversing
    changes made to 104503ad55e93df2fec489196cdf1c6ee53be76d.

---
+++ src/servo.c
@@ -0,0 +1,24 @@
+#include "utils.h"
+#include "delay.h"
+#include "drivers/gpio/gpio.h"
+#include "drivers/pwm/pwm.h"
+#include "bsp/nano.h"
+#include "utils/delay.h"
+int main(void) {
+    PWM_Init(GPIO_PORTB, 1, 50);
+    int i;
+    uint8_t duty = 13; // start la 0
+    int8_t step = 2;
+
+    while(1) {
+        PWM_SetDutyCycle(GPIO_PORTB, 1, duty);
+        Delay(500); 
+
+        duty += step;
+        if (duty >= 26) { // 180°
+            step = -2;
+        } else if (duty <= 13) { // 0°
+            step = 2;
+        }
+    }
+}
